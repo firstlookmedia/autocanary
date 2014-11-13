580d9e6f99a3eb74137b1d32af64f69b7297915a
Micah Lee <micah@micahflee.com>
2014-11-12 17:33:33 -0800

    packaged for OSX

--- BUILD.md
+++ BUILD.md
@@ -1,5 +1,25 @@
 # Building AutoCanary
 
+## Mac OS X
+
+To install the right dependencies, you need homebrew and pip installed on your Mac. Follow instructions at http://brew.sh/ to install homebrew, and run `sudo easy_install pip` to install pip.
+
+The first time you're setting up your dev environment:
+
+```sh
+brew install qt4 pyqt
+sudo pip install py2app
+```
+
+To build the .app:
+
+```sh
+python setup.py py2app
+```
+
+Now you should have `dist/AutoCanary.app`.
+
+
 ## Windows
 
 ### Setting up your dev environment
@@ -8,6 +28,7 @@
 * Right click on Computer, go to Properties. Click "Advanced system settings". Click Environment Variables. Under "System variables" double-click on Path to edit it. Add `;C:\Python27;C:\Python27\Scripts` to the end. Now you can just type `python` to run python scripts in the command prompt.
 * Go to http://www.riverbankcomputing.com/software/pyqt/download and download the latest PyQt4 for Windows for python 2.7, 32-bit (I downloaded `PyQt4-4.11-gpl-Py2.7-Qt4.8.6-x32.exe`), then install it.
 
+... these instructions are not done yet.
 
 
 ## Linux
--- setup.py
+++ setup.py
@@ -8,17 +8,37 @@ except ImportError:
 
 version = open('version').read().strip()
 
-setup(
-    name='autocanary',
-    version=version,
-    author='Micah Lee',
-    author_email='micah.lee@firstlook.org',
-    packages=['autocanary'],
-    include_package_data=True,
-    scripts=['install/autocanary'],
-    data_files=[
-        (os.path.join(sys.prefix, 'share/applications'), ['install/autocanary.desktop']),
-        (os.path.join(sys.prefix, 'share/pixmaps'), ['install/autocanary.xpm']),
-        (os.path.join(sys.prefix, 'share/autocanary'), ['share/icon.png'])
-    ]
-)
+system = platform.system()
+
+if system == 'Linux':
+    setup(
+        name='autocanary',
+        version=version,
+        author='Micah Lee',
+        author_email='micah.lee@firstlook.org',
+        packages=['autocanary'],
+        include_package_data=True,
+        scripts=['install/autocanary'],
+        data_files=[
+            (os.path.join(sys.prefix, 'share/applications'), ['install/autocanary.desktop']),
+            (os.path.join(sys.prefix, 'share/pixmaps'), ['install/autocanary.xpm']),
+            (os.path.join(sys.prefix, 'share/autocanary'), ['share/icon.png'])
+        ]
+    )
+
+elif system == 'Darwin':
+    setup(
+        name='AutoCanary',
+        app=['autocanary-launcher.py'],
+        data_files=[],
+        options={
+            'py2app': {
+                'argv_emulation': True,
+                'iconfile':'install/icon.icns',
+                'includes': ['sip', 'PyQt4', 'PyQt4.QtCore', 'PyQt4.QtGui'],
+                'excludes': ['PyQt4.QtDesigner', 'PyQt4.QtNetwork', 'PyQt4.QtOpenGL', 'PyQt4.QtScript', 'PyQt4.QtSql', 'PyQt4.QtTest', 'PyQt4.QtWebKit', 'PyQt4.QtXml', 'PyQt4.phonon']
+            }
+        },
+        setup_requires=['py2app'],
+    )
+
