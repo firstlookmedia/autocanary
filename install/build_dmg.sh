bc4e789c923d69dec318d6c965ddd0316ff87936
Micah Lee <micah@micahflee.com>
2015-02-23 18:46:34 -0800

    add codesigning to build_dmg.sh

--- install/build_dmg.sh
+++ install/build_dmg.sh
@@ -6,6 +6,7 @@ DMG_TMP_NAME=$ROOT/dist/tmp.dmg
 DMG_NAME=$ROOT/dist/AutoCanary.dmg
 TITLE=AutoCanary
 VOLUME=/Volumes/$TITLE
+DEVELOPER_ID="Micah Lee"
 
 cd $ROOT
 
@@ -16,6 +17,22 @@ rm -rf $ROOT/dist &>/dev/null 2>&1
 # build the .app
 echo Building AutoCanary.app
 python setup.py py2app
+
+# codesign the app
+echo Codesigning all of the frameworks, and finally the .app itself
+codesign --force --verify --verbose --sign "$DEVELOPER_ID" $ROOT/dist/AutoCanary.app/Contents/Frameworks/QtCore.framework/Versions/4/QtCore
+codesign --force --verify --verbose --sign "$DEVELOPER_ID" $ROOT/dist/AutoCanary.app/Contents/Frameworks/QtDeclarative.framework/Versions/4/QtDeclarative
+codesign --force --verify --verbose --sign "$DEVELOPER_ID" $ROOT/dist/AutoCanary.app/Contents/Frameworks/QtGui.framework/Versions/4/QtGui
+codesign --force --verify --verbose --sign "$DEVELOPER_ID" $ROOT/dist/AutoCanary.app/Contents/Frameworks/QtHelp.framework/Versions/4/QtHelp
+codesign --force --verify --verbose --sign "$DEVELOPER_ID" $ROOT/dist/AutoCanary.app/Contents/Frameworks/QtMultimedia.framework/Versions/4/QtMultimedia
+codesign --force --verify --verbose --sign "$DEVELOPER_ID" $ROOT/dist/AutoCanary.app/Contents/Frameworks/QtNetwork.framework/Versions/4/QtNetwork
+codesign --force --verify --verbose --sign "$DEVELOPER_ID" $ROOT/dist/AutoCanary.app/Contents/Frameworks/QtScript.framework/Versions/4/QtScript
+codesign --force --verify --verbose --sign "$DEVELOPER_ID" $ROOT/dist/AutoCanary.app/Contents/Frameworks/QtScriptTools.framework/Versions/4/QtScriptTools
+codesign --force --verify --verbose --sign "$DEVELOPER_ID" $ROOT/dist/AutoCanary.app/Contents/Frameworks/QtSql.framework/Versions/4/QtSql
+codesign --force --verify --verbose --sign "$DEVELOPER_ID" $ROOT/dist/AutoCanary.app/Contents/Frameworks/QtSvg.framework/Versions/4/QtSvg
+codesign --force --verify --verbose --sign "$DEVELOPER_ID" $ROOT/dist/AutoCanary.app/Contents/Frameworks/QtXmlPatterns.framework/Versions/4/QtXmlPatterns
+codesign --force --verify --verbose --sign "$DEVELOPER_ID" $ROOT/dist/AutoCanary.app/Contents/Frameworks/libQtCLucene.4.8.6.dylib
+codesign --force --verify --verbose --sign "$DEVELOPER_ID" $ROOT/dist/AutoCanary.app
 
 # create the .dmg
 echo Creating DMG
