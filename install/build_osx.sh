e8163068e119e70dcc42e13fafa01262085f33b8
Micah Lee <micah@micahflee.com>
2015-05-26 15:31:12 -0400

    updated build_osx.sh to only codesign if you pass --sign

--- install/build_osx.sh
+++ install/build_osx.sh
@@ -2,8 +2,6 @@
 
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 TITLE=AutoCanary
-SIGNING_IDENTITY_APP="Developer ID Application: Micah Lee"
-SIGNING_IDENTITY_INSTALLER="Developer ID Installer: Micah Lee"
 
 cd $ROOT
 
@@ -15,10 +13,17 @@ rm -rf $ROOT/dist &>/dev/null 2>&1
 echo Building AutoCanary.app
 python setup.py py2app
 
-# codesign the app
-echo Codesigning and building installer
-python $ROOT/install/prepare_for_codesign.py
-cd $ROOT/dist
-codesign --deep --verbose --sign "$SIGNING_IDENTITY_APP" AutoCanary.app
-productbuild --component AutoCanary.app /Applications AutoCanary.pkg --sign "$SIGNING_IDENTITY_INSTALLER"
-cd $ROOT
+if [ "$1" = "--sign" ]; then
+
+  SIGNING_IDENTITY_APP="Developer ID Application: Micah Lee"
+  SIGNING_IDENTITY_INSTALLER="Developer ID Installer: Micah Lee"
+
+  # codesign the app
+  echo Codesigning and building installer
+  python $ROOT/install/prepare_for_codesign.py
+  cd $ROOT/dist
+  codesign --deep --verbose --sign "$SIGNING_IDENTITY_APP" AutoCanary.app
+  productbuild --component AutoCanary.app /Applications AutoCanary.pkg --sign "$SIGNING_IDENTITY_INSTALLER"
+  cd $ROOT
+
+fi
