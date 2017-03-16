009e8870e43342ab04049d60d927827e3135cf16
Micah Lee <micah@micahflee.com>
2017-03-16 15:32:32 -0700

    Fix for jusing feedparser in python3

--- autocanary/headlines.py
+++ autocanary/headlines.py
@@ -17,6 +17,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import feedparser
+# Avoid feedparser python3 bug, see https://github.com/manolomartinez/greg/issues/47
+feedparser.PREFERRED_XML_PARSERS.remove('drv_libxml2')
 
 from . import common
 
